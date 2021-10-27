1a455345a2d2e2cbf28cfa3fb340cf14261ccb9b
chenqinhao <qhchen96@gmail.com>
2021-10-28 01:15:33 +0800

    初步目录规划

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,21 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-module.exports = {
-  presets: [
-    [
-      'taro',
-      {
-        framework: 'react',
-        ts: true
-      },
-    ],
+export const presets = [
+  [
+    'taro',
+    {
+      framework: 'react',
+      ts: true
+    },
   ],
-  plugins: [
-    [
-      'import',
-      {
-        libraryName: 'taro-hooks',
-        camel2DashComponentName: false
-      },
-      'taro-hooks',
-    ]
-  ],
-};
+];
+export const plugins = [
+  [
+    'import',
+    {
+      libraryName: 'taro-hooks',
+      camel2DashComponentName: false
+    },
+    'taro-hooks',
+  ]
+];
